acbdc6adb5d5ba379abb3ca373234c58d0489983
antonie <53366054+antonie-riziki@users.noreply.github.com>
2025-03-07 10:47:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np 
 import seaborn as sb 
 import matplotlib.pyplot as plt 
-import plotly.express as px
+# import plotly.express as px
 import os
 import csv
 import sys
